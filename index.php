ea7e04cfe274a49682b6fca6c3f7fdbd36f62edf
Paolo Montano <Paolo.Montano@Gmail.com>
2016-01-20 22:49:20 -0800

    Coming soon splash screen.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+    <title>Paolo Montano</title>
+    <style type="text/css">
+        body {
+            padding: 0px;
+            margin: 0px;
+        }
+        #wrapper {
+            width: 358px;
+            height: auto;
+            height: auto;
+            margin: 25% auto;
+        }
+    </style>
+</head>
+
+<body>
+<div id="wrapper">
+    <img src="images/splash.png" alt="Paolo Montano - Coming Soon">
+</div>
+</body>
